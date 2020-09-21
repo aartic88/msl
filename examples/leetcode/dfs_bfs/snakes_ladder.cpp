0b3bc799208f094e9929042f9751219f7c841ad3
aartic88 <aartic88@gmail.com>
2020-09-21 23:34:57 +0530

    snakes and ladderRemake

--- examples/leetcode/dfs_bfs/snakes_ladder.cpp
+++ examples/leetcode/dfs_bfs/snakes_ladder.cpp
@@ -53,67 +53,96 @@ int bfs(const vector<vector<int>> &snakeLadderMatrix)
     return -1;
 }
 */
+typedef pair<int,int> Edge;
+typedef vector<Edge> EdgeList;
+typedef vector<EdgeList> AdjList;
+#define VISITED 1
+#define UNVISITED 0
 
-typedef vector<vector<int>> AdjList;
+vector<int> convert2Dto1D(vector<vector<int>> &snakeLadderMatrix){
+    int n = snakeLadderMatrix.size();
+    for(int i=1; i<n;i+=2){
+        if(n%2==0)
+        reverse(snakeLadderMatrix[i].begin(),snakeLadderMatrix[i].end());
+        else
+        reverse(snakeLadderMatrix[i-1].begin(),snakeLadderMatrix[i-1].end());
+    }
+    vector<int> matrix1Dimage;
+    for(int i=n-1;i>=0;i--)
+        for(int j=n-1; j>=0;j--)
+            matrix1Dimage.push_back(snakeLadderMatrix[i][j]);
+    return matrix1Dimage;
+}
 
 AdjList makeAdjList(vector<vector<int>> &snakeLadderMatrix)
 {
     int n = snakeLadderMatrix.size();
-    AdjList graphAL(n * n);
-    for (int i = 0; i < n * n; i++)
-    {
-            graphAL[i].push_back(i + 1);
-    }
-    int count = n * n;
-    bool flag = true;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < snakeLadderMatrix[i].size(); j++)
-        {
-            if (flag)
-                count--;
-            else
-                count++;
-            if (snakeLadderMatrix[i][j] != -1)
-            {
-                graphAL[count].push_back(snakeLadderMatrix[i][j]);
+    AdjList graphAL(n*n);
+    vector<int> matrix1DImage = convert2Dto1D(snakeLadderMatrix);
+    
+    Edge e;
+    for(int i=0; i<n*n; i++){
+        e.first=i+1;
+        for(int j=i+1;j<=i+6 && j<n*n; j++){
+            if(matrix1DImage[i]!=-1){
+                e.second=matrix1DImage[i];
+                graphAL[i].push_back(e);
+                break;
+            }
+            else{
+                if(matrix1DImage[j]==-1){
+                    e.second=j+1;
+                    graphAL[i].push_back(e);
+                }
+                else{
+                    e.second=matrix1DImage[j];
+                    graphAL[i].push_back(e);
+                }
             }
         }
-        flag = !flag;
-        count = count - n;
-        if (!flag)
-            count--;
-        else
-            count++;   //addition of unsigned offset overflow.......
     }
     return graphAL;
 }
-int bfs (const AdjList & graph, int numVertex, int startingVertex) {
+vector<int> bfs (const AdjList & graph, int numVertex, int startingVertex) {
     // inside int main()---no recursion
     vector<int> distance(numVertex, INT32_MAX);
     distance[startingVertex] = 0;
     queue<int> q; 
-    q.push(startingVertex);
-    
+    q.push(startingVertex+1);
+    // distance from source s to s is 0
+    // start from source
     while (!q.empty()) {
         int u = q.front(); q.pop();
-        
-        for (int j = 0; j < (int)graph[u].size(); j++) {
-            int e = graph[u][j];
+        // queue: layer by layer!
+        for (int j = 0; j < (int)graph[u-1].size(); j++) {
+            Edge e = graph[u-1][j];
             // for each neighbor of u
-            if (distance[e] == INT32_MAX) {
+            int otherVertex  = u == e.first ? e.second : e.first;
+            if (distance[otherVertex-1] == INT32_MAX) {
                 // if v.first is unvisited + reachable
-                distance[e] = distance[u] + 1;
+                distance[otherVertex-1] = distance[u-1] + 1;
                 // make d[v.first] != INF to flag it
-                q.push(e);
+                q.push(otherVertex);
                 // enqueue v.first for the next iteration
             }
         }
     }
-    return distance[numVertex-1];
+    return distance;
 }
+TEST(testGraph, basicShortestPath0)
+{
+    vector<vector<int>> snakeLadderMatrix = {{-1, -1, -1, -1, },
+                                             {5, -1, -1, -1, },
+                                             {-1, -1, 15, -1, },
+                                             {-1, 11, -1, -1, }};
+    AdjList graphAL = makeAdjList(snakeLadderMatrix);
+    auto distance = bfs(graphAL, 16, 0);
+    for (int i = 0; i<distance.size(); i++) {
+        cout << i <<" " << distance[i] << endl;
+    }
 
-TEST(testGraph, basicShortestPath)
+} 
+TEST(testGraph, basicShortestPath1)
 {
     vector<vector<int>> snakeLadderMatrix = {{-1, -1, -1, -1, -1, -1},
                                              {-1, -1, -1, -1, -1, -1},
@@ -122,8 +151,10 @@ TEST(testGraph, basicShortestPath)
                                              {-1, -1, -1, -1, -1, -1},
                                              {-1, 15, -1, -1, -1, -1}};
     AdjList graphAL = makeAdjList(snakeLadderMatrix);
-    auto distance = bfs(graphAL,36,1);
-    cout<<distance<<endl;
+    auto distance = bfs(graphAL, 36, 0);
+    for (int i = 0; i<distance.size(); i++) {
+        cout << i <<" " << distance[i] << endl;
+    }
 }
 TEST(testGraph, basicShortestPath2)
 {
@@ -132,7 +163,19 @@ TEST(testGraph, basicShortestPath2)
         {-1, 3},
     };
     AdjList graphAL = makeAdjList(snakeLadderMatrix);
-    auto distance = bfs(graphAL,4,1);
-    cout<<distance<<endl;
+    auto distance = bfs(graphAL, 4, 0);
+    for (int i = 0; i<distance.size(); i++) {
+        cout << i <<" " << distance[i] << endl;
+    }
 }
-
+TEST(testGraph, basicShortestPath3)
+{
+    vector<vector<int>> snakeLadderMatrix = {{4, -1, -1},
+                                             {-1, -1, -1},
+                                             {-1, 8, -1}};
+    AdjList graphAL = makeAdjList(snakeLadderMatrix);
+    auto distance = bfs(graphAL, 9, 0);
+    for (int i = 0; i<distance.size(); i++) {
+        cout << i <<" " << distance[i] << endl;
+    }
+}
